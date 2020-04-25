bc2bc1bc5ef086df0047b84c877b4ac374571062
Rummy43 <33029685+Rummy43@users.noreply.github.com>
2020-04-25 21:04:52 +0530

    Added my project

---
+++ src/main/java/com/project/SortingAPI/SortingApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.SortingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortingApiApplication.class, args);
+	}
+
+}
