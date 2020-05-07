78a508f230ec58bed4246f3f5f79a024a8af7fce
Rummy43 <33029685+Rummy43@users.noreply.github.com>
2020-05-07 23:35:11 +0530

    partial part of First testcase.

---
+++ src/main/java/com/project/SortingAPI/Service/SortingService.java
@@ -0,0 +1,14 @@
+package com.project.SortingAPI.Service;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.project.SortingAPI.model.Employee;
+
+
+@Service
+public class SortingService {
+
+	public List<Employee> findAll() {
+		return new ArrayList<>();
+	}
+}
---
+++ src/main/java/com/project/SortingAPI/controller/SortingController.java
@@ -0,0 +1,8 @@
+package com.project.SortingAPI.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SortingController {
+
+}
